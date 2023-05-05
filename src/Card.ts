4ce7e935a1cfe9af140c35c7264a59ed61467c3b
KauaRodrigo <kaurodrigo.joenck2@gmail.com>
2023-05-04 21:26:30 -0300

    inicio testes

---
+++ src/Card.ts
@@ -0,0 +1,8 @@
+export default class Card {
+    
+    constructor(readonly title: string, readonly estimative: number) {
+        if(title === "") throw new Error("Título é obrigatório")
+        if(estimative < 0) throw new Error("Estimativa deve ser positiva")
+    }
+
+}
---
+++ test/Column.test.ts
@@ -0,0 +1,11 @@
+import Column from "../src/Column";
+
+test("Deve criar uma coluna", function() {
+    const column = new Column("Coluna A", true);
+    expect(column.name).toBe("Coluna A");
+    expect(column.hasEstimative).toBeTruthy()
+})
+
+test("Não deve criar coluna sem nome", function() {
+    expect(() => new Column("", true)).toThrow(new Error("Nome é obrigatório"))
+})
